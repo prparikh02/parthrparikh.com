6520d0a99e882befad9de4f90e567d689eb47e5a
parth <prparikh02@gmail.com>
2017-10-27 22:16:54 -0400

    Minimum Viable Page

--- parthrparikh/settings.py
+++ parthrparikh/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = prod_config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = prod_config.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = prod_config.ALLOWED_HOSTS
 
 
 # Application definition
